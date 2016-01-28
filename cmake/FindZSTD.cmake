b024bca38e752d4db9215ee0bd6059044a734f2b
Francesc Alted <faltet@gmail.com>
2016-01-28 17:39:51 +0100

    Initial trial at zstd support

---
+++ cmake/FindZSTD.cmake
@@ -0,0 +1,10 @@
+find_path(ZSTD_INCLUDE_DIR zstd.h)
+
+find_library(ZSTD_LIBRARY NAMES zstd)
+
+if (ZSTD_INCLUDE_DIR AND ZSTD_LIBRARY)
+    set(ZSTD_FOUND TRUE)
+    message(STATUS "Found ZSTD library: ${ZSTD_LIBRARY}")
+else ()
+    message(STATUS "No ZSTD library found.  Using internal sources.")
+endif ()
