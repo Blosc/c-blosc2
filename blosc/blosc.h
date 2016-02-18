03095873968e941d6b371b927c346834fd193cb2
Francesc Alted <faltet@gmail.com>
2016-02-18 17:01:44 +0100

    State the difference between compcode and compformat more clearly

--- blosc/blosc.h
+++ blosc/blosc.h
@@ -80,7 +80,7 @@ extern "C" {
 #define BLOSC_ZLIB_COMPNAME      "zlib"
 #define BLOSC_ZSTD_COMPNAME      "zstd"
 
-/* Codes for the different compression libraries shipped with Blosc */
+/* Codes for compression libraries shipped with Blosc (code must be < 8) */
 #define BLOSC_BLOSCLZ_LIB    0
 #define BLOSC_LZ4_LIB        1
 #define BLOSC_SNAPPY_LIB     2
@@ -95,7 +95,7 @@ extern "C" {
 #define BLOSC_ZLIB_LIBNAME      "Zlib"
 #define BLOSC_ZSTD_LIBNAME      "Zstd"
 
-/* The codes for compressor formats shipped with Blosc (code must be < 8) */
+/* The codes for compressor formats shipped with Blosc */
 #define BLOSC_BLOSCLZ_FORMAT  BLOSC_BLOSCLZ_LIB
 #define BLOSC_LZ4_FORMAT      BLOSC_LZ4_LIB
 /* LZ4HC and LZ4 share the same format */
