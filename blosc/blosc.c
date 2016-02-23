Blosc/c-blosc2

commit 143e9523b3286c40272c6ef4cbc14e144ef4cbfc

More uniform increase of blocksize with clevel

blosc/blosc.c
@@ -934,8 +934,6 @@ static int32_t compute_blocksize(struct blosc_context* context,
     return 1;
   }
 
-  blocksize = nbytes;           /* Start by a whole buffer as blocksize */
-
   if (forced_blocksize) {
     blocksize = forced_blocksize;
     /* Check that forced blocksize is not too small */
@@ -986,6 +984,8 @@ static int32_t compute_blocksize(struct blosc_context* context,
       blocksize *= 2;
       break;
     case 7:
+      blocksize *= 4;
+      break;
     case 8:
       blocksize *= 8;
       break;

commit 990fe60e191cc9afd622d885d4811e69a1940f08

Fix a bug introduced in 143e952

blosc/blosc.c
@@ -951,7 +951,7 @@ static int32_t compute_blocksize(struct blosc_context* context,
                                  int32_t typesize,
                                  int32_t nbytes,
                                  int32_t forced_blocksize) {
-  int32_t blocksize;
+  int32_t blocksize = nbytes;
 
   /* Protection against very small buffers */
   if (nbytes < (int32_t)typesize) {