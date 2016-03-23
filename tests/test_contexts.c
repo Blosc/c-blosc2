60264cad53b08629220dd88830bce80e431367db
Francesc Alted <faltet@gmail.com>
2016-03-23 18:45:41 +0100

    Added a test for contexts

--- tests/test_contexts.c
+++ tests/test_contexts.c
@@ -1,40 +1,25 @@
 /*
-    Copyright (C) 2014  Francesc Alted
-    http://blosc.org
-    License: MIT (see LICENSE.txt)
-
-    Example program demonstrating the use of a Blosc from C code.
-
-    To compile this program:
-
-    $ gcc -O contexts.c -o contexts -lblosc
-
-    To run:
-
-    $ ./contexts
-    Blosc version info: 2.0.0a2 ($Date:: 2016-01-08 #$)
-    Compression: 40000000 -> 999393 (40.0x)
-    Correctly extracted 5 elements from compressed chunk!
-    Decompression succesful!
-    Succesful roundtrip!
+  Copyright (C) 2015  Francesc Alted
+  http://blosc.org
+  License: MIT (see LICENSE.txt)
 
 */
 
 #include <stdio.h>
-#include "blosc.h"
+#include "test_common.h"
 
-#define SIZE 10 * 1000 * 1000
+#define SIZE 500 * 1000
 #define NTHREADS 2
 
 
 int main() {
-  static float data[SIZE];
-  static float data_out[SIZE];
-  static float data_dest[SIZE];
-  float data_subset[5];
-  float data_subset_ref[5] = {5, 6, 7, 8, 9};
-  int isize = SIZE * sizeof(float), osize = SIZE * sizeof(float);
-  int dsize = SIZE * sizeof(float), csize;
+  static int32_t data[SIZE];
+  static int32_t data_out[SIZE];
+  static int32_t data_dest[SIZE];
+  int32_t data_subset[5];
+  int32_t data_subset_ref[5] = {5, 6, 7, 8, 9};
+  int isize = SIZE * sizeof(int32_t), osize = SIZE * sizeof(int32_t);
+  int dsize = SIZE * sizeof(int32_t), csize;
   int i, ret;
   blosc2_context_cparams cparams = BLOSC_CPARAMS_DEFAULTS;
   blosc2_context_dparams dparams = BLOSC_DPARAMS_DEFAULTS;
@@ -49,7 +34,7 @@ int main() {
          BLOSC_VERSION_STRING, BLOSC_VERSION_DATE);
 
   /* Create a context for compression */
-  cparams.typesize = sizeof(float);
+  cparams.typesize = sizeof(int32_t);
   cparams.compcode = BLOSC_BLOSCLZ;
   cparams.filtercode = BLOSC_SHUFFLE;
   cparams.clevel = 5;
@@ -60,14 +45,12 @@ int main() {
   csize = blosc2_compress_ctx(cctx, isize, data, data_out, osize);
   if (csize == 0) {
     printf("Buffer is uncompressible.  Giving up.\n");
-    return 1;
+    return EXIT_FAILURE;
   }
   else if (csize < 0) {
     printf("Compression error.  Error code: %d\n", csize);
-    return csize;
+    return EXIT_FAILURE;
   }
-
-  printf("Compression: %d -> %d (%.1fx)\n", isize, csize, (1. * isize) / csize);
 
   /* Create a context for decompression */
   dparams.nthreads = NTHREADS;
@@ -76,37 +59,33 @@ int main() {
   ret = blosc2_getitem_ctx(dctx, data_out, 5, 5, data_subset);
   if (ret < 0) {
     printf("Error in blosc2_getitem_ctx().  Giving up.\n");
-    return 1;
+    return EXIT_FAILURE;
   }
 
   for (i = 0; i < 5; i++) {
     if (data_subset[i] != data_subset_ref[i]) {
       printf("blosc2_getitem_ctx() fetched data differs from original!\n");
-      return -1;
+      return EXIT_FAILURE;
     }
   }
-  printf("Correctly extracted 5 elements from compressed chunk!\n");
 
   /* Decompress  */
   dsize = blosc2_decompress_ctx(dctx, data_out, data_dest, dsize);
   if (dsize < 0) {
     printf("Decompression error.  Error code: %d\n", dsize);
-    return dsize;
+    return EXIT_FAILURE;
   }
-
-  printf("Decompression succesful!\n");
 
   for (i = 0; i < SIZE; i++) {
     if (data[i] != data_dest[i]) {
       printf("Decompressed data differs from original!\n");
-      return -1;
+      return EXIT_FAILURE;
     }
   }
-  printf("Succesful roundtrip!\n");
 
-  /* Release resources */
+  /* Free resources */
   blosc2_free_ctx(cctx);
   blosc2_free_ctx(dctx);
 
-  return 0;
+  return EXIT_SUCCESS;
 }
