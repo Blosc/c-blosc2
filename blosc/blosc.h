376099e5eb657420e21a9babb0bb8e3a68ea64ee
Francesc Alted <faltet@gmail.com>
2016-03-28 18:34:00 +0200

    Bumping to 2.0.0 alpha 3

--- blosc/blosc.h
+++ blosc/blosc.h
@@ -22,7 +22,7 @@ extern "C" {
 #define BLOSC_VERSION_MINOR    0    /* for minor interface/format changes  */
 #define BLOSC_VERSION_RELEASE  0    /* for tweaks, bug-fixes, or development */
 
-#define BLOSC_VERSION_STRING   "2.0.0a2"  /* string version.  Sync with above! */
+#define BLOSC_VERSION_STRING   "2.0.0a3"  /* string version.  Sync with above! */
 #define BLOSC_VERSION_REVISION "$Rev$"   /* revision version */
 #define BLOSC_VERSION_DATE     "$Date:: 2016-01-08 #$"    /* date version */
 
