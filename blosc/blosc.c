560a8b567771b627dce6e03bfa6bc22509420651
Francesc Alted <faltet@gmail.com>
2017-08-21 02:58:43 +0200

    Better use of conditional

--- blosc/blosc.c
+++ blosc/blosc.c
@@ -1003,14 +1003,13 @@ static int parallel_blosc(blosc2_context* context) {
   /* Synchronization point for all threads (wait for finalization) */
   WAIT_FINISH(-1, context);
 
-  if (context->thread_giveup_code > 0) {
-    /* Return the total bytes (de-)compressed in threads */
-    return context->num_output_bytes;
-  }
-  else {
+  if (context->thread_giveup_code <= 0) {
     /* Compression/decompression gave up.  Return error code. */
     return context->thread_giveup_code;
   }
+
+  /* Return the total bytes (de-)compressed in threads */
+  return context->num_output_bytes;
 }
 
 
