d46acc809831559e790819d8bedea87057a53272
Francesc Alted <faltet@gmail.com>
2017-11-06 13:46:08 +0100

    [CHORE] Add an example of dictionary compression

---
+++ examples/zstd_dict.c
@@ -0,0 +1,113 @@
+/*
+  Copyright (C) 2017  Francesc Alted
+  http://blosc.org
+  License: BSD (see LICENSE.txt)
+
+  Example program demonstrating use of the Blosc filter from C code.
+
+  To compile this program:
+
+  $ gcc -O zstd_dict.c -o zstd_dict -lblosc
+
+  To run:
+
+  $ ./zstd_dict
+  TODO ...
+
+*/
+
+#include <stdio.h>
+#include <assert.h>
+#include <time.h>
+#include "blosc.h"
+
+#define KB  1024.
+#define MB  (1024*KB)
+#define GB  (1024*MB)
+
+#define CHUNKSIZE (200 * 1000)
+#define NCHUNKS 5
+#define NTHREADS 1
+
+
+int main() {
+  static int64_t data[CHUNKSIZE];
+  static int64_t data_dest[CHUNKSIZE];
+  const size_t isize = CHUNKSIZE * sizeof(int64_t);
+  int dsize = 0;
+  int64_t nbytes, cbytes;
+  blosc2_cparams cparams = BLOSC_CPARAMS_DEFAULTS;
+  blosc2_dparams dparams = BLOSC_DPARAMS_DEFAULTS;
+  blosc2_schunk* schunk;
+  int i;
+  int nchunk;
+  size_t nchunks;
+  blosc_timestamp_t last, current;
+  double ttotal;
+
+  printf("Blosc version info: %s (%s)\n",
+         BLOSC_VERSION_STRING, BLOSC_VERSION_DATE);
+
+  /* Initialize the Blosc compressor */
+  blosc_init();
+
+  /* Create a super-chunk container */
+  cparams.typesize = 8;
+  cparams.filters[0] = BLOSC_DELTA;
+  cparams.compcode = BLOSC_ZSTD;
+  cparams.use_dict = 1;
+  cparams.clevel = 1;
+  cparams.nthreads = NTHREADS;
+  dparams.nthreads = NTHREADS;
+  schunk = blosc2_new_schunk(cparams, dparams);
+
+  blosc_set_timestamp(&last);
+  for (nchunk = 1; nchunk <= NCHUNKS; nchunk++) {
+    for (i = 0; i < CHUNKSIZE; i++) {
+      data[i] = i * (int64_t)nchunk;
+    }
+    nchunks = blosc2_append_buffer(schunk, isize, data);
+    assert(nchunks == nchunk);
+  }
+  /* Gather some info */
+  nbytes = schunk->nbytes;
+  cbytes = schunk->cbytes;
+  blosc_set_timestamp(&current);
+  ttotal = blosc_elapsed_secs(last, current);
+  printf("Compression ratio: %.1f MB -> %.1f MB (%.1fx)\n",
+         nbytes / MB, cbytes / MB, (1. * nbytes) / cbytes);
+  printf("Compression time: %.3g s, %.1f MB/s\n",
+         ttotal, nbytes / (ttotal * MB));
+
+  /* Retrieve and decompress the chunks (0-based count) */
+  blosc_set_timestamp(&last);
+  for (nchunk = NCHUNKS-1; nchunk >= 0; nchunk--) {
+    dsize = blosc2_decompress_chunk(schunk, (size_t)nchunk,
+                                    (void *)data_dest, isize);
+  }
+  if (dsize < 0) {
+    printf("Decompression error.  Error code: %d\n", dsize);
+    return dsize;
+  }
+  blosc_set_timestamp(&current);
+  ttotal = blosc_elapsed_secs(last, current);
+  printf("Decompression time: %.3g s, %.1f MB/s\n",
+         ttotal, nbytes / (ttotal * MB));
+
+  /* Check integrity of the first chunk */
+  for (i = 0; i < CHUNKSIZE; i++) {
+    if (data_dest[i] != (uint64_t)i) {
+      printf("Decompressed data differs from original %d, %zd!\n",
+             i, data_dest[i]);
+      return -1;
+    }
+  }
+
+  printf("Successful roundtrip!\n");
+
+  /* Free resources */
+  blosc2_destroy_schunk(schunk);
+  blosc_destroy();
+
+  return 0;
+}
