abd5fd360d771b46bd66575b9836fd66901e3488
Francesc Alted <faltet@gmail.com>
2019-08-08 17:32:05 +0200

    Fix an issue introduced in previous commit

--- blosc/blosc2.c
+++ blosc/blosc2.c
@@ -359,18 +359,18 @@ static int lz4hc_wrap_compress(const char* input, size_t input_length,
 
 static int lz4_wrap_decompress(const char* input, size_t compressed_length,
                                char* output, size_t maxout) {
-  int cbytes;
+  int nbytes;
 #ifdef HAVE_IPP
   int outlen = (int)maxout;
   int inlen = (int)compressed_length;
   IppStatus status;
   status = ippsDecodeLZ4_8u((const Ipp8u*)input, inlen, (Ipp8u*)output, &outlen);
   //status = ippsDecodeLZ4Dict_8u((const Ipp8u*)input, &inlen, (Ipp8u*)output, 0, &outlen, NULL, 1 << 16);
-  cbytes = (status == ippStsNoErr) ? inlen : -inlen;
+  nbytes = (status == ippStsNoErr) ? outlen : -outlen;
 #else
-  cbytes = LZ4_decompress_safe(input, output, compressed_length, (int)maxout);
+  nbytes = LZ4_decompress_safe(input, output, (int)compressed_length, (int)maxout);
 #endif
-  if (cbytes != (int)compressed_length) {
+  if (nbytes != (int)maxout) {
     return 0;
   }
   return (int)maxout;
