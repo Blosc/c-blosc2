97477aeded641e7a1483813b0070a8b0462f48bf
Francesc Alted <faltet@gmail.com>
2019-08-30 10:39:03 +0200

    Post 2.0.0-beta.2 release actions done

--- blosc/blosc2.h
+++ blosc/blosc2.h
@@ -33,7 +33,7 @@ extern "C" {
 #define BLOSC_VERSION_MINOR    0    /* for minor interface/format changes  */
 #define BLOSC_VERSION_RELEASE  0    /* for tweaks, bug-fixes, or development */
 
-#define BLOSC_VERSION_STRING   "2.0.0-beta.2"  /* string version.  Sync with above! */
+#define BLOSC_VERSION_STRING   "2.0.0-beta.3.dev"  /* string version.  Sync with above! */
 #define BLOSC_VERSION_REVISION "$Rev$"   /* revision version */
 #define BLOSC_VERSION_DATE     "$Date:: 2019-08-30 #$"    /* date version */
 
