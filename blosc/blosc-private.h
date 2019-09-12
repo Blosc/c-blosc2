72cb2e4a85c3e7611b795227b9cc80c250b9bd98
Francesc Alted <francesc@blosc.org>
2019-09-12 12:07:51 +0200

    Endianness (#84)
    * New is_little_endian() function
    
    * Store info about endianness in chunk header

--- blosc/blosc-private.h
+++ blosc/blosc-private.h
@@ -22,27 +22,39 @@ extern "C" {
 
 *********************************************************************/
 
+// Return true if platform is little endian; else false
+static bool is_little_endian(void) {
+  bool little;
+  int i = 1;
+  char* p = (char*)&i;
+
+  if (p[0] == 1) {
+    little = true;
+  }
+  else {
+    little = false;
+  }
+  return little;
+}
+
 /* Copy 4 bytes from @p *pa to int32_t, changing endianness if necessary. */
 static inline int32_t sw32_(const void* pa) {
   int32_t idest;
   uint8_t* dest = (uint8_t*)&idest;
   uint8_t* pa_ = (uint8_t*)pa;
-  int i = 1;                    /* for big/little endian detection */
-  char* p = (char*)&i;
 
-  if (p[0] != 1) {
-    /* big endian */
-    dest[0] = pa_[3];
-    dest[1] = pa_[2];
-    dest[2] = pa_[1];
-    dest[3] = pa_[0];
-  }
-  else {
-    /* little endian */
+  bool little_endian = is_little_endian();
+  if (little_endian) {
     dest[0] = pa_[0];
     dest[1] = pa_[1];
     dest[2] = pa_[2];
     dest[3] = pa_[3];
+  }
+  else {
+    dest[0] = pa_[3];
+    dest[1] = pa_[2];
+    dest[2] = pa_[1];
+    dest[3] = pa_[0];
   }
   return idest;
 }
@@ -51,25 +63,21 @@ static inline int32_t sw32_(const void* pa) {
 static inline void _sw32(void* dest, int32_t a) {
   uint8_t* dest_ = (uint8_t*)dest;
   uint8_t* pa = (uint8_t*)&a;
-  int i = 1;                    /* for big/little endian detection */
-  char* p = (char*)&i;
 
-  if (p[0] != 1) {
-    /* big endian */
-    dest_[0] = pa[3];
-    dest_[1] = pa[2];
-    dest_[2] = pa[1];
-    dest_[3] = pa[0];
-  }
-  else {
-    /* little endian */
+  bool little_endian = is_little_endian();
+  if (little_endian) {
     dest_[0] = pa[0];
     dest_[1] = pa[1];
     dest_[2] = pa[2];
     dest_[3] = pa[3];
   }
+  else {
+    dest_[0] = pa[3];
+    dest_[1] = pa[2];
+    dest_[2] = pa[1];
+    dest_[3] = pa[0];
+  }
 }
-
 
 #ifdef __cplusplus
 }
