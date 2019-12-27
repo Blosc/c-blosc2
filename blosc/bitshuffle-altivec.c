2c10bda0a1714ec18ef5afa8acfb23dbfe694af7
Francesc Alted <faltet@gmail.com>
2019-12-27 10:38:24 +0100

    Use byte transpose routines explicitly

--- blosc/bitshuffle-altivec.c
+++ blosc/bitshuffle-altivec.c
@@ -21,6 +21,7 @@
   #error ALTIVEC is not supported by the target architecture/platform and/or this compiler.
 #endif
 #include <altivec.h>
+#include "transpose-altivec.h"
 
 #include <emmintrin.h>
 #include <stdio.h>
@@ -238,28 +239,20 @@ int64_t bitshuffle1_altivec(void* src, void* dest, const size_t size,  const siz
   return count;
 }
 
-static const __vector uint8_t even = (const __vector uint8_t) {
-  0x00, 0x02, 0x04, 0x06, 0x08, 0x0a, 0x0c, 0x0e,
-  0x10, 0x12, 0x14, 0x16, 0x18, 0x1a, 0x1c, 0x1e};
-
-static const __vector uint8_t odd = (const __vector uint8_t) {
-  0x01, 0x03, 0x05, 0x07, 0x09, 0x0b, 0x0d, 0x0f,
-  0x11, 0x13, 0x15, 0x17, 0x19, 0x1b, 0x1d, 0x1f};
-
 /* Transpose bytes within elements for 16 bit elements. */
 int64_t bshuf_trans_byte_elem_16(void* in, void* out, const size_t size) {
   static const uint8_t bytesoftype = 2;
-  __vector uint8_t xmm0[2], xmm1[2];
+  __vector uint8_t xmm0[2];
 
   for (int i = 0; i + 15 < size; i += 16) {
     for (int j = 0; j < bytesoftype; j++)
       xmm0[j] = vec_xl(bytesoftype * i + 16 * j, (const uint8_t*)in);
 
-    xmm1[0] = vec_perm(xmm0[0], xmm0[1], even);
-    xmm1[1] = vec_perm(xmm0[0], xmm0[1], odd);
+    /* Transpose vectors */
+    transpose2x16(xmm0);
 
     for (int j = 0; j < bytesoftype; j++)
-      vec_xst(xmm1[j], i + j * size, (uint8_t*)out);
+      vec_xst(xmm0[j], i + j * size, (uint8_t*)out);
   }
   return bshuf_trans_byte_elem_remainder(in, out, size, bytesoftype,
                                          size - size % 16);
