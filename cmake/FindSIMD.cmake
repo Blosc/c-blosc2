8689c2789cf70f5ed52cc2529d96633073998d48
Francesc Alted <faltet@gmail.com>
2020-04-24 14:00:49 +0200

    Do not run AVX2 tests unless the local CPU has support for it

---
+++ cmake/FindSIMD.cmake
@@ -0,0 +1,58 @@
+# Check if SSE/AVX instructions are available on the machine where
+# the project is compiled.
+
+IF(CMAKE_SYSTEM_NAME MATCHES "Linux")
+   EXEC_PROGRAM(cat ARGS "/proc/cpuinfo" OUTPUT_VARIABLE CPUINFO)
+
+   STRING(REGEX REPLACE "^.*(sse2).*$" "\\1" SSE_THERE ${CPUINFO})
+   STRING(COMPARE EQUAL "sse2" "${SSE_THERE}" SSE2_TRUE)
+   IF (SSE2_TRUE)
+      set(SSE2_FOUND true CACHE BOOL "SSE2 available on host")
+   ELSE (SSE2_TRUE)
+      set(SSE2_FOUND false CACHE BOOL "SSE2 available on host")
+   ENDIF (SSE2_TRUE)
+
+   STRING(REGEX REPLACE "^.*(avx2).*$" "\\1" SSE_THERE ${CPUINFO})
+   STRING(COMPARE EQUAL "avx2" "${SSE_THERE}" AVX2_TRUE)
+   IF (AVX2_TRUE)
+      set(AVX2_FOUND true CACHE BOOL "AVX2 available on host")
+   ELSE (AVX2_TRUE)
+      set(AVX2_FOUND false CACHE BOOL "AVX2 available on host")
+   ENDIF (AVX2_TRUE)
+
+ELSEIF(CMAKE_SYSTEM_NAME MATCHES "Darwin")
+   EXEC_PROGRAM("/usr/sbin/sysctl -a | grep machdep.cpu.features" OUTPUT_VARIABLE CPUINFO)
+   STRING(REGEX REPLACE "^.*[^S](SSE2).*$" "\\1" SSE_THERE ${CPUINFO})
+   STRING(COMPARE EQUAL "SSE2" "${SSE_THERE}" SSE2_TRUE)
+   IF (SSE2_TRUE)
+      set(SSE2_FOUND true CACHE BOOL "SSE2 available on host")
+   ELSE (SSE2_TRUE)
+      set(SSE2_FOUND false CACHE BOOL "SSE2 available on host")
+   ENDIF (SSE2_TRUE)
+
+   EXEC_PROGRAM("/usr/sbin/sysctl -a | grep machdep.cpu.leaf7_features" OUTPUT_VARIABLE CPUINFO)
+   STRING(REGEX REPLACE "^.*(AVX2).*$" "\\1" SSE_THERE ${CPUINFO})
+   STRING(COMPARE EQUAL "AVX2" "${SSE_THERE}" AVX2_TRUE)
+   IF (AVX2_TRUE)
+      set(AVX2_FOUND true CACHE BOOL "AVX2 available on host")
+   ELSE (AVX2_TRUE)
+      set(AVX2_FOUND false CACHE BOOL "AVX2 available on host")
+   ENDIF (AVX2_TRUE)
+
+ELSEIF(CMAKE_SYSTEM_NAME MATCHES "Windows")
+   # TODO.  For now supposing SSE2 is safe enough
+   set(SSE2_FOUND   true  CACHE BOOL "SSE2 available on host")
+   set(AVX2_FOUND   false CACHE BOOL "AVX2 available on host")
+ELSE(CMAKE_SYSTEM_NAME MATCHES "Linux")
+   set(SSE2_FOUND   true  CACHE BOOL "SSE2 available on host")
+   set(AVX2_FOUND false CACHE BOOL "AVX2 available on host")
+ENDIF(CMAKE_SYSTEM_NAME MATCHES "Linux")
+
+if(NOT SSE2_FOUND)
+      MESSAGE(STATUS "Could not find hardware support for SSE2 on this machine.")
+endif(NOT SSE2_FOUND)
+if(NOT AVX2_FOUND)
+      MESSAGE(STATUS "Could not find hardware support for AVX2 on this machine.")
+endif(NOT AVX2_FOUND)
+
+mark_as_advanced(SSE2_FOUND AVX2_FOUND)
