24ba4bf8de6a1cf675d6bd6fbc9443c2f5d1479e
Nathan Moinvaziri <nathan@nathanm.com>
2020-04-28 18:07:29 -0700

    Standardize no-space after if(), else(), endif(), foreach(), and endforeach().

--- cmake/FindLIZARD.cmake
+++ cmake/FindLIZARD.cmake
@@ -2,9 +2,9 @@ find_path(LIZARD_INCLUDE_DIR lizard_common.h)
 
 find_library(LIZARD_LIBRARY NAMES lizard)
 
-if (LIZARD_INCLUDE_DIR AND LIZARD_LIBRARY)
+if(LIZARD_INCLUDE_DIR AND LIZARD_LIBRARY)
     set(LIZARD_FOUND TRUE)
     message(STATUS "Found LIZARD library: ${LIZARD_LIBRARY}")
-else ()
+else()
     message(STATUS "No LIZARD library found.  Using internal sources.")
-endif ()
+endif()
