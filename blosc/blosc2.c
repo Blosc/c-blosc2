dca55210b29deb7c31d3a31d906c1cf85aa5bf77
Nathan Moinvaziri <nathan@nathanm.com>
2020-06-24 20:05:45 -0700

    Fixed asan heap-buffer-overflow due to destsize not being checked.

--- blosc/blosc2.c
+++ blosc/blosc2.c
@@ -1347,6 +1347,11 @@ static int initialize_context_compression(
             BLOSC_MAX_BUFFERSIZE);
     return -1;
   }
+  if (destsize < BLOSC_MAX_OVERHEAD) {
+    fprintf(stderr, "Output buffer size should be larger than %d bytes\n",
+            BLOSC_MAX_OVERHEAD);
+    return -1;
+  }
 
   /* Compression level */
   if (clevel < 0 || clevel > 9) {
