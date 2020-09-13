d80b5acaf23ec4c2453a348b35096b99601fcde5
Nathan Moinvaziri <nathan@nathanm.com>
2020-09-12 18:36:18 -0700

    Fixed wrong size being passed to blosc_run_decompression_with_context.

--- blosc/blosc2.c
+++ blosc/blosc2.c
@@ -2190,7 +2190,7 @@ int blosc2_decompress(const void* src, int32_t srcsize, void* dest, int32_t dest
   pthread_mutex_lock(&global_comp_mutex);
 
   result = blosc_run_decompression_with_context(
-          g_global_context, src, INT32_MAX, dest, destsize);
+          g_global_context, src, srcsize, dest, destsize);
 
   pthread_mutex_unlock(&global_comp_mutex);
 
