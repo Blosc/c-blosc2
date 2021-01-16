f384c79adecc779b4abcac07779fe47c9f20081f
Francesc Alted <francesc@blosc.org>
2021-01-16 15:32:28 +0100

    A more complete check of special values

--- bench/zero_runlen.c
+++ bench/zero_runlen.c
@@ -13,7 +13,6 @@
 #include <math.h>
 
 #include "blosc2.h"
-#include "frame.h"
 
 
 #define KB  1024
@@ -24,36 +23,25 @@
 #define CHUNKSIZE (500 * 1000)  // > NCHUNKS for the bench purposes
 #define NTHREADS 4
 
-#define ACTIVATE_ZERO_DETECTION false
-#define CHECK_NAN true
-#define CHECK_VALUE false
+enum {
+  ZERO_DETECTION = 0,
+  CHECK_ZEROS = 1,
+  CHECK_NANS = 2,
+  CHECK_VALUES = 3,
+};
 #define REPEATED_VALUE 1
 
 
-void fill_buffer(int32_t *buffer) {
-  for (int i = 0; i < CHUNKSIZE; i++) {
-    if (CHECK_VALUE) {
-      buffer[i] = REPEATED_VALUE;
-    }
-    else if (CHECK_NAN) {
-      *(float*)(buffer + i) = nanf("");
-    }
-    else {
-      buffer[i] = 0;
-    }
-//    if (i == CHUNKSIZE - 1) {
-//      buffer[i] = 1;
-//    }
-  }
-}
-
-
-int main(void) {
+int check_special_values(int svalue) {
   blosc2_schunk *schunk;
   int32_t isize = CHUNKSIZE * sizeof(int32_t);
-  int dsize;
+  int32_t osize = CHUNKSIZE * sizeof(int32_t) + BLOSC_MAX_OVERHEAD;
+  int dsize, csize;
   int64_t nbytes, frame_len;
   int nchunk, nchunks = 0;
+  int rc;
+  int32_t value = REPEATED_VALUE;
+  float fvalue;
   blosc_timestamp_t last, current;
   double totaltime;
   double totalsize = (double)isize * NCHUNKS;
@@ -79,32 +67,36 @@ int main(void) {
   blosc_set_timestamp(&last);
   void* chunk = malloc(BLOSC_EXTENDED_HEADER_LENGTH + sizeof(int32_t));
   for (nchunk = 0; nchunk < NCHUNKS; nchunk++) {
-    if (ACTIVATE_ZERO_DETECTION) {
-      fill_buffer(data_buffer);
-      nchunks = blosc2_schunk_append_buffer(schunk, data_buffer, isize);
-    }
-    else {
-      int csize;
-      if (CHECK_VALUE) {
-        int32_t value = REPEATED_VALUE;
+    switch (svalue) {
+      case ZERO_DETECTION:
+        memset(data_buffer, 0, isize);
+        csize = blosc2_compress(5, 1, sizeof(int32_t), data_buffer, isize, chunk, osize);
+        break;
+      case CHECK_ZEROS:
+        csize = blosc2_chunk_zeros(isize, sizeof(int32_t), chunk, BLOSC_EXTENDED_HEADER_LENGTH);
+        break;
+      case CHECK_NANS:
+        csize = blosc2_chunk_nans(isize, sizeof(float), chunk, BLOSC_EXTENDED_HEADER_LENGTH);
+        break;
+      case CHECK_VALUES:
         csize = blosc2_chunk_repeatval(isize, sizeof(int32_t), chunk,
                                        BLOSC_EXTENDED_HEADER_LENGTH + sizeof(int32_t), &value);
-      }
-      else if (CHECK_NAN) {
-          csize = blosc2_chunk_nans(isize, sizeof(float), chunk, BLOSC_EXTENDED_HEADER_LENGTH);
-      } else {
-        csize = blosc2_chunk_zeros(isize, sizeof(int32_t), chunk, BLOSC_EXTENDED_HEADER_LENGTH);
-      }
-      if (csize < 0) {
-        printf("Error creating chunk: %d\n", csize);
-      }
-      nchunks = blosc2_schunk_append_chunk(schunk, chunk, true);
+        break;
+      default:
+        printf("Unrecognized case");
+        exit(1);
     }
+    if (csize < 0) {
+      printf("Error creating chunk: %d\n", csize);
+      exit(1);
+    }
+    nchunks = blosc2_schunk_append_chunk(schunk, chunk, true);
     if (nchunks < 0) {
       printf("Error appending chunk: %d\n", nchunks);
-      return -1;
+      exit(1);
     }
   }
+
   blosc_set_timestamp(&current);
   free(chunk);
   totaltime = blosc_elapsed_secs(last, current);
@@ -124,7 +116,7 @@ int main(void) {
     dsize = blosc2_schunk_decompress_chunk(schunk, nchunk, rec_buffer, isize);
     if (dsize < 0) {
       printf("Decompression error.  Error code: %d\n", dsize);
-      return dsize;
+      exit(dsize);
     }
     assert (dsize == (int)isize);
   }
@@ -140,35 +132,45 @@ int main(void) {
   for (nchunk = 0; nchunk < NCHUNKS; nchunk++) {
     bool needs_free;
     uint8_t* chunk_;
-    int csize = blosc2_schunk_get_chunk(schunk, nchunk, &chunk_, &needs_free);
+    csize = blosc2_schunk_get_chunk(schunk, nchunk, &chunk_, &needs_free);
     if (csize < 0) {
       printf("blosc2_schunk_get_chunk error.  Error code: %d\n", dsize);
       return csize;
     }
-    int expected_csize = CHECK_VALUE ? BLOSC_EXTENDED_HEADER_LENGTH + sizeof(int32_t) : BLOSC_EXTENDED_HEADER_LENGTH;
-    assert (csize == expected_csize);
-    if (CHECK_VALUE) {
-      int32_t value;
-      int rc = blosc_getitem(chunk_, nchunk, 1, &value);
-      if (rc < 0) {
-        printf("Error in getitem of a special value\n");
-        return rc;
+    switch (svalue) {
+      case CHECK_VALUES:
+        rc = blosc_getitem(chunk_, nchunk, 1, &value);
+        if (rc < 0) {
+          printf("Error in getitem of a special value\n");
+          return rc;
+        }
+        if (value != REPEATED_VALUE) {
+          printf("Wrong value!");
+          exit(1);
+        }
+        break;
+      case CHECK_NANS:
+        rc = blosc_getitem(chunk_, nchunk, 1, &fvalue);
+        if (rc < 0) {
+          printf("Error in getitem of a special value\n");
+          return rc;
+        }
+        if (!isnan(fvalue)) {
+          printf("Wrong value!");
+          exit(1);
+        }
+        break;
+      default:
+        // It can only be zeros
+        rc = blosc_getitem(chunk_, nchunk, 1, &value);
+        if (rc < 0) {
+          printf("Error in getitem of a special value\n");
+          return rc;
+        }
+        if (value != 0) {
+        printf("Wrong value!");
+        exit(1);
       }
-      assert (value == REPEATED_VALUE);
-    }
-    else if (CHECK_NAN) {
-      float value;
-      int rc = blosc_getitem(chunk_, nchunk, 1, &value);
-      if (rc < 0) {
-        printf("Error in getitem of a special value\n");
-        return rc;
-      }
-      assert (isnan(value));
-    }
-    else {
-      int32_t value;
-      blosc_getitem(chunk_, nchunk, 1, &value);
-      assert (value == 0);
     }
     if (needs_free) {
       free(chunk_);
@@ -178,44 +180,44 @@ int main(void) {
   totaltime = blosc_elapsed_secs(last, current);
   printf("[getitem] Elapsed time:\t %6.3f s.\n", totaltime);
 
-  /* Check that all the values have a good roundtrip */
-  blosc_set_timestamp(&last);
-  for (nchunk = 0; nchunk < NCHUNKS; nchunk++) {
-    dsize = blosc2_schunk_decompress_chunk(schunk, nchunk, (void *) rec_buffer, isize);
-    if (dsize < 0) {
-      printf("Decompression error.  Error code: %d\n", dsize);
-      return dsize;
-    }
-    assert (dsize == (int)isize);
-    if (CHECK_VALUE) {
-      int32_t* buffer = (int32_t*)rec_buffer;
-      for (int i = 0; i < CHUNKSIZE; i++) {
-        if (buffer[i] != REPEATED_VALUE) {
-          printf("Value is not correct in chunk %d, position: %d\n", nchunk, i);
-          return -1;
-        }
-      }
-    }
-    else if (CHECK_NAN) {
-      float* buffer = (float*)rec_buffer;
-      for (int i = 0; i < CHUNKSIZE; i++) {
-        if (!isnan(buffer[i])) {
-          printf("Value is not correct in chunk %d, position: %d\n", nchunk, i);
-          return -1;
-        }
-      }
-    }
-    else {
-      int32_t* buffer = (int32_t*)rec_buffer;
-      for (int i = 0; i < CHUNKSIZE; i++) {
-        if (buffer[i] != 0) {
-          printf("Value is not correct in chunk %d, position: %d\n", nchunk, i);
-          return -1;
-        }
-      }
-    }
-  }
-  printf("All data did a good roundtrip!\n");
+//  /* Check that all the values have a good roundtrip */
+//  blosc_set_timestamp(&last);
+//  for (nchunk = 0; nchunk < NCHUNKS; nchunk++) {
+//    dsize = blosc2_schunk_decompress_chunk(schunk, nchunk, (void *) rec_buffer, isize);
+//    if (dsize < 0) {
+//      printf("Decompression error.  Error code: %d\n", dsize);
+//      return dsize;
+//    }
+//    assert (dsize == (int)isize);
+//    if (CHECK_VALUE) {
+//      int32_t* buffer = (int32_t*)rec_buffer;
+//      for (int i = 0; i < CHUNKSIZE; i++) {
+//        if (buffer[i] != REPEATED_VALUE) {
+//          printf("Value is not correct in chunk %d, position: %d\n", nchunk, i);
+//          return -1;
+//        }
+//      }
+//    }
+//    else if (CHECK_NAN) {
+//      float* buffer = (float*)rec_buffer;
+//      for (int i = 0; i < CHUNKSIZE; i++) {
+//        if (!isnan(buffer[i])) {
+//          printf("Value is not correct in chunk %d, position: %d\n", nchunk, i);
+//          return -1;
+//        }
+//      }
+//    }
+//    else {
+//      int32_t* buffer = (int32_t*)rec_buffer;
+//      for (int i = 0; i < CHUNKSIZE; i++) {
+//        if (buffer[i] != 0) {
+//          printf("Value is not correct in chunk %d, position: %d\n", nchunk, i);
+//          return -1;
+//        }
+//      }
+//    }
+//  }
+//  printf("All data did a good roundtrip!\n");
 
   /* Free resources */
   free(data_buffer);
@@ -226,4 +228,27 @@ int main(void) {
   blosc_destroy();
 
   return 0;
+}
+
+
+int main(void) {
+  int rc;
+  // TODO: Check why this is not working...
+//  printf("Testing zero detection...");
+//  check_special_values(ZERO_DETECTION);
+  printf("*** Testing special zeros...");
+  rc = check_special_values(CHECK_ZEROS);
+  if (rc < 0) {
+    return rc;
+  }
+  printf("*** Testing special NaNs...");
+  rc = check_special_values(CHECK_NANS);
+  if (rc < 0) {
+    return rc;
+  }
+  printf("*** Testing special values...");
+  rc = check_special_values(CHECK_VALUES);
+  if (rc < 0) {
+    return rc;
+  }
 }
