876468a066f8da9e67d2e14bc03a5abb55f1d457
martaiborra <martaiborra24@gmail.com>
2021-02-05 11:34:49 +0100

    Implement bench comparative between frame and sframe

---
+++ bench/sframe_bench.c
@@ -0,0 +1,376 @@
+/*********************************************************************
+  Benchmark for testing sframe vs frame.
+
+  For usage instructions of this benchmark, please see:
+
+    http://blosc.org/synthetic-benchmarks.html
+
+  I'm collecting speeds for different machines, so the output of your
+  benchmarks and your processor specifications are welcome!
+
+  Author: The Blosc Developers <blosc@blosc.org>
+
+  See LICENSE.txt for details about copyright and rights to use.
+**********************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "blosc2.h"
+#include <assert.h>
+
+#define KB  1024
+#define MB  (1024*KB)
+#define GB  (1024*MB)
+
+#define NCHUNKS 1000       /* number of chunks */
+#define CHUNKSIZE (200 * 1000)
+
+int nchunks = NCHUNKS;
+
+#if defined(_WIN32)
+#include <malloc.h>
+
+#endif  /* defined(_WIN32) && !defined(__MINGW32__) */
+
+
+
+void test_update(blosc2_schunk* schunk_sframe, blosc2_schunk* schunk_frame) {
+  blosc_timestamp_t last, current;
+  double frame_update_time, sframe_update_time;
+
+  size_t isize = CHUNKSIZE * sizeof(int32_t);
+  int32_t* data = malloc(isize);
+
+  // Random update list
+  int32_t* update_chunks = malloc(sizeof(int32_t) * 5);
+  srand(time(NULL));
+  for (int i = 0; i < 5; i++) {
+    update_chunks[i] = rand() % schunk_sframe->nchunks;
+  }
+
+  printf("*******************************************************\n");
+  printf("******************* Updating 5 chunks ******************\n");
+  printf("*******************************************************\n");
+
+  // Update the sframe chunks
+  sframe_update_time = 0.0;
+  frame_update_time = 0.0;
+  int32_t datasize = sizeof(int32_t) * CHUNKSIZE;
+  int32_t chunksize = sizeof(int32_t) * CHUNKSIZE + BLOSC_MAX_OVERHEAD;
+  uint8_t* chunk;
+  int csize;
+  int _nchunks;
+  for (int i = 0; i < 5; i++) {
+    // Generate data
+    for (int j = 0; j < CHUNKSIZE; j++) {
+      data[j] = i * CHUNKSIZE;
+    }
+    chunk = malloc(chunksize);
+    csize = blosc2_compress_ctx(schunk_sframe->cctx, data, datasize, chunk, chunksize);
+    if (csize < 0) {
+      printf("ERROR: chunk cannot be compressed\n");
+    }
+    // Sframe
+    blosc_set_timestamp(&current);
+    _nchunks = blosc2_schunk_update_chunk(schunk_sframe, update_chunks[i], chunk, true);
+    blosc_set_timestamp(&last);
+    if (_nchunks < 0){
+      printf("ERROR: chunk cannot be updated correctly\n");
+    }
+    sframe_update_time += blosc_elapsed_secs(current, last);
+
+    // Frame
+    blosc_set_timestamp(&current);
+    _nchunks = blosc2_schunk_update_chunk(schunk_frame, update_chunks[i], chunk, true);
+    blosc_set_timestamp(&last);
+    if (_nchunks <= 0){
+      printf("ERROR: chunk cannot be updated correctly\n");
+    }
+    frame_update_time += blosc_elapsed_secs(current, last);
+
+    free(chunk);
+  }
+
+  printf("[Sframe Update] Elapsed time:\t %6.3f s.  Total sframe size: %l bytes \n", sframe_update_time, schunk_sframe->nbytes);
+  printf("[Frame Update] Elapsed time:\t %6.3f s.  Total frame size: %l bytes \n", frame_update_time, schunk_frame->nbytes);
+
+  /* Remove directory */
+  blosc2_remove_dir(schunk_sframe->storage->urlpath);
+  /* Free blosc resources */
+  free(update_chunks);
+  free(datasize);
+  free(data);
+  blosc2_schunk_free(schunk_sframe);
+  blosc2_schunk_free(schunk_frame);
+  /* Destroy the Blosc environment */
+  blosc_destroy();
+}
+
+void test_insert(blosc2_schunk* schunk_sframe, blosc2_schunk* schunk_frame) {
+  blosc_timestamp_t last, current;
+  double frame_insert_time, sframe_insert_time;
+
+  size_t isize = CHUNKSIZE * sizeof(int32_t);
+  int32_t* data = malloc(isize);
+
+  // Random insert list
+  int32_t* insert_chunks = malloc(sizeof(int32_t) * 5);
+  srand(time(NULL));
+  for (int i = 0; i < 5; i++) {
+    insert_chunks[i] = rand() % schunk_sframe->nchunks;
+  }
+
+  printf("*******************************************************\n");
+  printf("****************** Inserting 5 chunks *****************\n");
+  printf("*******************************************************\n");
+
+  // Update the sframe chunks
+  sframe_insert_time = 0.0;
+  frame_insert_time = 0.0;
+  int32_t datasize = sizeof(int32_t) * CHUNKSIZE;
+  int32_t chunksize = sizeof(int32_t) * CHUNKSIZE + BLOSC_MAX_OVERHEAD;
+  uint8_t* chunk;
+  int csize;
+  int _nchunks;
+  for (int i = 0; i < 5; i++) {
+    // Generate data
+    for (int j = 0; j < CHUNKSIZE; j++) {
+      data[j] = j + i * CHUNKSIZE;
+    }
+    chunk = malloc(chunksize);
+    csize = blosc2_compress_ctx(schunk_sframe->cctx, data, datasize, chunk, chunksize);
+    if (csize < 0) {
+      printf("ERROR: chunk cannot be compressed\n");
+    }
+    // Sframe
+    blosc_set_timestamp(&current);
+    _nchunks = blosc2_schunk_insert_chunk(schunk_sframe, insert_chunks[i], chunk, true);
+    blosc_set_timestamp(&last);
+    if (_nchunks < 0){
+      printf("ERROR: chunk cannot be updated correctly\n");
+    }
+    sframe_insert_time += blosc_elapsed_secs(current, last);
+
+    // Frame
+    blosc_set_timestamp(&current);
+    _nchunks = blosc2_schunk_update_chunk(schunk_frame, insert_chunks[i], chunk, true);
+    blosc_set_timestamp(&last);
+    if (_nchunks <= 0){
+      printf("ERROR: chunk cannot be updated correctly\n");
+    }
+    frame_insert_time += blosc_elapsed_secs(current, last);
+
+    free(chunk);
+  }
+
+  printf("[Sframe Update] Elapsed time:\t %6.3f s.  Total sframe size: %l bytes \n", sframe_insert_time, schunk_sframe->nbytes);
+  printf("[Frame Update] Elapsed time:\t %6.3f s.  Total frame size: %l bytes \n", frame_insert_time, schunk_frame->nbytes);
+
+  /* Remove directory */
+  blosc2_remove_dir(schunk_sframe->storage->urlpath);
+  /* Free blosc resources */
+  free(insert_chunks);
+  free(datasize);
+  free(data);
+  blosc2_schunk_free(schunk_sframe);
+  blosc2_schunk_free(schunk_frame);
+  /* Destroy the Blosc environment */
+  blosc_destroy();
+}
+
+
+void test_reorder(blosc2_schunk* schunk_sframe, blosc2_schunk* schunk_frame) {
+  blosc_timestamp_t last, current;
+  double frame_reorder_time, sframe_reorder_time;
+
+  // Reorder list
+  int *offsets_order = malloc(sizeof(int) * schunk_sframe->nchunks);
+  for (int i = 0; i < schunk_sframe->nchunks; ++i) {
+    offsets_order[i] = (i + 3) % schunk_sframe->nchunks;
+  }
+
+  printf("*******************************************************\n");
+  printf("****************** Reordering chunks ******************\n");
+  printf("*******************************************************\n");
+
+  // Reorder sframe
+  blosc_set_timestamp(&current);
+  int err = blosc2_schunk_reorder_offsets(schunk_sframe, offsets_order);
+  blosc_set_timestamp(&last);
+  if (err < 0) {
+    printf("ERROR: cannot reorder chunks\n");
+  }
+  sframe_reorder_time = blosc_elapsed_secs(current, last);
+
+  // Reorder frame
+  blosc_set_timestamp(&current);
+  err = blosc2_schunk_reorder_offsets(schunk_frame, offsets_order);
+  blosc_set_timestamp(&last);
+  if (err < 0) {
+    printf("ERROR: cannot reorder chunks\n");
+  }
+  frame_reorder_time = blosc_elapsed_secs(current, last);
+
+
+
+  printf("[Sframe Update] Elapsed time:\t %f s.  Total sframe size: %l bytes \n", sframe_reorder_time, schunk_sframe->nbytes);
+  printf("[Frame Update] Elapsed time:\t %f s.  Total frame size: %l bytes \n", frame_reorder_time, schunk_frame->nbytes);
+
+  /* Remove directory */
+  blosc2_remove_dir(schunk_sframe->storage->urlpath);
+  /* Free blosc resources */
+  free(offsets_order);
+  blosc2_schunk_free(schunk_sframe);
+  blosc2_schunk_free(schunk_frame);
+  /* Destroy the Blosc environment */
+  blosc_destroy();
+}
+
+void test_create_sframe_frame(char* operation, int32_t iter) {
+  blosc_timestamp_t last, current;
+  double frame_append_time, sframe_append_time, frame_decompress_time, sframe_decompress_time;
+
+  int64_t nbytes, cbytes;
+  size_t isize = CHUNKSIZE * sizeof(int32_t);
+  int dsize;
+  float totalsize = (float)(isize * nchunks);
+  int32_t* data = malloc(isize);
+  int32_t* data_dest = malloc(isize);
+
+  blosc2_cparams cparams = BLOSC2_CPARAMS_DEFAULTS;
+  blosc2_dparams dparams = BLOSC2_DPARAMS_DEFAULTS;
+
+  blosc2_schunk* schunk_sframe;
+  blosc2_schunk* schunk_frame;
+
+  /* Initialize the Blosc compressor */
+  blosc_init();
+  printf("*******************************************************\n");
+  printf("***** Creating the frame and sframe with %d chunks ****\n", nchunks);
+  printf("*******************************************************\n");
+  /* Create a frame container */
+  cparams.typesize = sizeof(int32_t);
+
+  cparams.nthreads = 1;
+  dparams.nthreads = 1;
+
+  blosc2_storage storage = {.sequential=false, .urlpath="dir.b2sframe", .cparams=&cparams, .dparams=&dparams};
+  schunk_sframe = blosc2_schunk_new(storage);
+
+  blosc2_storage storage2 = {.sequential=true, .urlpath="test_frame.b2frame", .cparams=&cparams, .dparams=&dparams};
+  schunk_frame = blosc2_schunk_new(storage2);
+
+  printf("Test comparation frame vs sframe with %d chunks.\n", nchunks);
+  // Feed it with data
+  sframe_append_time=0.0;
+  frame_append_time=0.0;
+  blosc_set_timestamp(&current);
+  for (int nchunk = 0; nchunk < nchunks; nchunk++) {
+    for (int i = 0; i < CHUNKSIZE; i++) {
+      data[i] = i + nchunk * CHUNKSIZE;
+    }
+    blosc_set_timestamp(&current);
+    blosc2_schunk_append_buffer(schunk_sframe, data, isize);
+    blosc_set_timestamp(&last);
+    sframe_append_time += blosc_elapsed_secs(current, last);
+
+    blosc_set_timestamp(&current);
+    blosc2_schunk_append_buffer(schunk_frame, data, isize);
+    blosc_set_timestamp(&last);
+    frame_append_time += blosc_elapsed_secs(current, last);
+  }
+  printf("[Sframe Compr] Elapsed time:\t %6.3f s.  Processed data: %.3f GB (%.3f GB/s)\n",
+         sframe_append_time, totalsize / GB, totalsize / (GB * sframe_append_time));
+  printf("[Frame Compr] Elapsed time:\t %6.3f s.  Processed data: %.3f GB (%.3f GB/s)\n",
+         frame_append_time, totalsize / GB, totalsize / (GB * frame_append_time));
+
+  /* Gather some info */
+  nbytes = schunk_sframe->nbytes;
+  cbytes = schunk_sframe->cbytes;
+  printf("Compression super-chunk-sframe: %ld -> %ld (%.1fx)\n",
+         (long)nbytes, (long)cbytes, (1. * nbytes) / cbytes);
+  nbytes = schunk_frame->nbytes;
+  cbytes = schunk_frame->cbytes;
+  printf("Compression super-chunk-frame: %ld -> %ld (%.1fx)\n",
+         (long)nbytes, (long)cbytes, (1. * nbytes) / cbytes);
+
+  // Decompress the data
+  // sframe
+  blosc_set_timestamp(&current);
+  for (int nchunk = 0; nchunk < nchunks; nchunk++) {
+    dsize = blosc2_schunk_decompress_chunk(schunk_sframe, nchunk, (void *) data_dest, isize);
+    if (dsize < 0) {
+      printf("Decompression error sframe.  Error code: %d\n", dsize);
+    }
+    assert (dsize == (int)isize);
+  }
+  blosc_set_timestamp(&last);
+  sframe_decompress_time = blosc_elapsed_secs(current, last);
+  // frame
+  blosc_set_timestamp(&current);
+  for (int nchunk = 0; nchunk < nchunks; nchunk++) {
+    dsize = blosc2_schunk_decompress_chunk(schunk_frame, nchunk, (void *) data_dest, isize);
+    if (dsize < 0) {
+      printf("Decompression error frame.  Error code: %d\n", dsize);
+    }
+    assert (dsize == (int)isize);
+  }
+  blosc_set_timestamp(&last);
+  frame_decompress_time = blosc_elapsed_secs(current, last);
+
+  printf("[Sframe Decompr] Elapsed time:\t %6.3f s.  Processed data: %.3f GB (%.3f GB/s)\n",
+         sframe_decompress_time, totalsize / GB, totalsize / (GB * sframe_decompress_time));
+  printf("[Frame Decompr] Elapsed time:\t %6.3f s.  Processed data: %.3f GB (%.3f GB/s)\n",
+         frame_decompress_time, totalsize / GB, totalsize / (GB * frame_decompress_time));
+
+  printf("Decompression successful!\n");
+
+  printf("Successful roundtrip!\n");
+
+  /* Free blosc resources */
+  free(data_dest);
+  free(data);
+
+  // Run as much as iter tests
+  if (strcmp(operation, "insert") == 0) {
+    for (int i = 0; i < iter; i++) {
+      test_insert(schunk_sframe, schunk_frame);
+    }
+  }
+  else if (strcmp(operation, "update") == 0) {
+    for (int i = 0; i < iter; i++) {
+      test_update(schunk_sframe, schunk_frame);
+    }
+  }
+  else if (strcmp(operation, "reorder") == 0) {
+    // reorder
+    for (int i = 0; i < iter; i++) {
+      test_reorder(schunk_sframe, schunk_frame);
+    }
+  }
+
+}
+
+int main(int argc, char* argv[]) {
+  int iterations = 1;
+  char* operation;
+
+  if (argc >= 5) {
+    printf("Usage: ./sframe_bench [nchunks] [iterations] [insert | update | reorder]");
+    exit(1);
+  }
+  else if (argc >= 2) {
+    nchunks = (int)strtol(argv[1], NULL, 10);
+  }
+  if (argc >= 3) {
+    iterations = (int)strtol(argv[2], NULL, 10);
+  }
+  if (argc == 4) {
+    operation = argv[3];
+  }
+
+  test_create_sframe_frame(operation, iterations);
+
+  return 0;
+}
--- dframe.rst
+++ dframe.rst
@@ -1,34 +1,46 @@
-Blosc2 Extended-Frame
-=====================
+Blosc2  Sparse Frame
+====================
 
 Overview
 --------
-The extended-frame implementation allows the storage of different Blosc
-data chunks sparse on-disk. An extended-frame is in fact a frame, with the main difference that
-its chunk section is only composed of the index chunk. Instead, the data chunks
-are stored as independent binary files in the same directory where
-the frame file is stored.
+The sparse frame implementation allows the storage of different Blosc
+data chunks sparse on-disk. For this feature it is used a frame file, which
+was implemented for sequentially on-disk storage purposes.
 
-To understand it better, a graphic is shown bellow, comparing the
-structure of a frame and an eframe.
-A frame for sequentially on-disk storage purposes is a binary file
+A frame file is a binary file
 composed of a header, a chunks section and a trailer.
 The header contains information needed to decompress the chunks and the
 trailer contains a user meta data chunk and a fingerprint.
-As seen in the image, the chunks section is composed of all the
+As seen in the comparative image below (on the left the frame
+structure and on the right the sparse frame structure), the chunks section is composed of all the
 data chunks of the frame plus the index chunk. This last one contains
 the index to each chunk (where each chunk begins inside the frame file).
+All of this is stored sequentially, that means one part is followed
+by the next one without (initially) any empty spaces.
 
-However, the frame file of an extended-frame (named as chunks.b2frame)
-contains only the index chunk in its chunk section plus the header
-and the trailer as well. With the index chunk the name of the chunk
+However, in a sparse frame the chunks are already stored in a
+specified directory as independent binary files.
+But there is still the
+need to store the information to decompress the chunks
+as well as a place to store user meta data.
+This is stored in the `chunks.b2frame`,
+which is in fact a frame file with the main difference that its
+chunks section is only composed of the index chunk. Thus, as seen
+in the image following, the `chunks.b2frame`
+file is composed of the header, the index chunk and the trailer.
+The index chunk contains in each position the identifier
+of each chunk which is a number from 0 to :math:`2^29-1`.
+With that number the name of the chunk
 file is obtained by passing it to hexadecimal with a length of 8
 characters padded with zeros and adding the `.chunk` extension.
+For example, if the index chunk is 46 (2E in hexadecimal)
+the chunk file name would be
+`0000002E.chunk`.
 
-.. image:: C:\Users\Marta\Desktop\framevseframe.svg
-  :scale: 50 %
+.. image:: C:\Users\Marta\Desktop\framevsSframecopy.svg
+  :width: 70%
+  :align: center
 
-Image Comparative between Frame (left) and Disperse-Frame (right)
 
 Advantages
 ----------
@@ -38,47 +50,71 @@ a pretty reasonable number considering that the limitations of the most
 used file systems are between :math:`2^16` and :math:`2^32` files
 per directory.
 
-Another advantage compared to the frame is the lack of empty
+Another advantage compared with the frame is the lack of empty
 spaces when updating a chunk.
-When updating a chunk in a frame, the space from the old chunk
-is left useless and the new chunk is added at the end (just before
-the index chunk), whereas when updating an eframe
-the whole chunkfile is overwritten. To help to understand it
-a little more the frame and eframe can be seen as bookshelves.
-The first one is a fitted bookshelves. Whereas an eframe can be
-seen as regular bookshelves. Both of the bookshelves are wider
-enough to fit all the number of files (or books) that the file system
-can admit.
-Because the frame bookshelves are fitted, they are built to fit books
-of 30 cm tall. But what happens when updating a book by its new version
-is that the editors decided to change the height of the book to
-35 cm.
-The new version cannot fit in the bookshelves and an extra shelf is built
-to store it leaving the empty space from the old book useless.
-However, because the sframe bookshelves have a stantard height
-of 40 cm the  new book is replaced by the old one without any useless
-space left.
+To illustrate how a frame and a sparse frame behave when updating
+a chunk an example is used  for each case.
 
+The set of the data chunks from a frame could be structured as the
+`Jenga board game tower <https://en.wikipedia.org/wiki/Jenga>`_, a tower
+built with wood blocks but, in constrast to the genuine
+`Jenga board game`, not all the
+blocks have the same size. Below is showed the initially structure
+of this tower. If the orange piece is updated (changed by another
+piece) there are two possibilities. The first one is that the new piece
+fits into the empty space left where the old piece was. In that case,
+the new piece is put in the previous space without any problem.
+However, if the new piece
+does not fit into the empty space left, the new piece is placed at the
+top of the tower (like in the game) leaving the space
+where the old piece was empty.
 
+.. image:: C:\Users\Marta\Desktop\jenga3.svg
+  :width: 50%
+  :align: center
+
+On the other hand, the chunks of a sparse frame can be seen as books. So the
+chunks structure could be seen as a bookshelf in which each book
+is a different chunk. If it is needed to update one book with
+the new edition, one only has to grab the old edition and
+replace it by the new one. And the books on the right are moved
+so that there is the exact space needed for the new edition. In the
+following image the yellow book is replaced by the maroon with the
+green rim. Note there is not a single empty space between
+the books after replacing the book.
+
+.. image:: C:\Users\Marta\Desktop\bookshelf.svg
+  :width: 50%
+  :align: center
 
 Future work
 -----------
 
-In the future instead of storing the chunks in the local computer
+This implementation opens doors to another interesting features made
+with a little bit more of work.
+
+For example, adjusting the code to work in the network,
+instead of storing the chunks in a local computer
 they could be stored in another machine and accessed remotely.
-That way, with just the metainfo (frame file) we could
-access the whole eframe with its data chunks. For a clearer idea
-of how much this will mean an eframe of 1000 chunks was created. The
-total size of the data chunks was 58 MB and the frames file size was
+That way, with just the metainfo (the frame file) we could
+access the whole sparse frame with its data chunks.
+For a clearer idea
+of how much this will mean a sparse frame of 1000 chunks was created.
+The
+total size of the data chunks from this sparse frame
+was 58 MB and the frames file size was
 only 1 KB. This could be surely practical for teleworking. With just an
 email of something more than a 1 KB any worker could access all
-the data stored in the eframe.
+the data stored in the sparse frame.
 
 This feature could also be used in the future to implement
-remote databases based in . In which the key would be the metainfo plus
-chunk index and the value would be the data chunk.
-
-If you are interested in this and want to contribute any help is
-welcome (sponsoring, donations, ideas or even some implementations).
+remote databases. These databases could be composed
+of a key assigned to a value. The key is an identifier for each element
+of the database, therefore it must be unique. Whereas the value
+is just the information accessed to each key. Similar to a set of unique
+keys and a set of doors. Each key can only open one door in particular,
+but the room behind the door may or may not be an exact copy of another
+one. In this case, the key would be composed of the metainfo plus the index of the chunk
+(so that each key is unique) and the value would be the data chunk.
 
 
