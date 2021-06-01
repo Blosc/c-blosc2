fd86c63ac6aa33a5ebcf5ce9a4002ec0ca25f773
oscargm98 <soscargm98@gmail.com>
2021-06-01 15:23:44 +0200

    fixing Windows warnings

--- plugins/codecs/ndlz/test_ndlz.c
+++ plugins/codecs/ndlz/test_ndlz.c
@@ -99,7 +99,7 @@ static int test_ndlz_4(blosc2_schunk* schunk) {
             printf("Buffer is uncompressible.  Giving up.\n");
             return 0;
         } else if (csize < 0) {
-            printf("Compression error.  Error code: %ld\n", csize);
+            printf("Compression error.  Error code: %lld\n", csize);
             return (int) csize;
         }
         csize_f += csize;
@@ -108,7 +108,7 @@ static int test_ndlz_4(blosc2_schunk* schunk) {
         /* Decompress  */
         dsize = blosc2_decompress_ctx(dctx, data_out, chunksize + BLOSC_MAX_OVERHEAD, data_dest, chunksize);
         if (dsize <= 0) {
-            printf("Decompression error.  Error code: %ld\n", dsize);
+            printf("Decompression error.  Error code: %lld\n", dsize);
             return (int) dsize;
         }
         /*
@@ -134,7 +134,7 @@ static int test_ndlz_4(blosc2_schunk* schunk) {
     blosc2_free_ctx(dctx);
 
     printf("Succesful roundtrip!\n");
-    printf("Compression: %d -> %ld (%.1fx)\n", chunksize, csize_f, (1. * chunksize) / csize_f);
+    printf("Compression: %d -> %lld (%.1fx)\n", chunksize, csize_f, (1. * chunksize) / csize_f);
     return (int) (chunksize - csize_f);
 }
 
