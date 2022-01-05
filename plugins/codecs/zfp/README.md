8fba42bccfbc76fd53f7cff69b560c083d64731f
oscargm98 <soscargm98@gmail.com>
2022-01-05 14:27:14 +0100

    new ZFP mode FIXED_PRECISION added

--- plugins/codecs/zfp/README.md
+++ plugins/codecs/zfp/README.md
@@ -11,21 +11,25 @@ A lossy codec like ZFP allows for much better compression ratios at the expense 
 Plugin usage
 -------------------
 
-The codec consists of an encoder called `blosc2_zfp_acc_compress()` to codify data and
-a decoder called `blosc2_zfp_acc_decompress()` to recover the original data.
+The codec consists of different encoders to codify data and
+decoders to recover the original data, placed at `blosc2-zfp.c`.
 
 This plugin only works with 1 to 4-dim datasets of floats or doubles, so if one tries to work with another type of dataset, it will return an error value.
 
 The parameters used by *ZFP* are the ones specified in the `blosc2_codec` structure in the `blosc2.h` header.
-Furthermore, *ZFP* allows to work in two modes, BLOSC_CODEC_ZFP_FIXED_RATE and BLOSC_CODEC_ZFP_FIXED_ACCURACY, each of one can be fine-tuned via the `meta` parameter as follows:
+Furthermore, *ZFP* allows to work in three modes, BLOSC_CODEC_ZFP_FIXED_ACCURACY, BLOSC_CODEC_ZFP_FIXED_PRECISION and BLOSC_CODEC_ZFP_FIXED_RATE, each of one can be fine-tuned via the `meta` parameter as follows:
 
-- BLOSC_CODEC_ZFP_FIXED_RATE: `meta` must be a number between 1 and 100. It is called *ratio* and represents the size that the compressed cells must have based on the input cell size. For example, if the cell size is 2000 bytes and meta = 50, the output cell size will be 50% of 2000 = 1000 bytes.
 - BLOSC_CODEC_ZFP_FIXED_ACCURACY: `meta` is used as absolute error in truncation.  For example, if meta = -2, each value loss must be less than or equal to 10^(-2) = 0,01. Then, if 23,0567 is a value of the original input, after compressing and decompressing this input with meta = -2, this value must be between 23,0467 and 23,0667. As `meta` is unsigned in the codec plugin interface, if user does not want the compiler to show a warning, a cast to unsigned integer of 1 byte must be done before calling the plugin.
+- BLOSC_CODEC_ZFP_FIXED_PRECISION: `meta` must be a number between 1 and ZFP_MAX_PREC. It is called *precision* and represents the maximum number of bit planes encoded, which means the number of most significant uncompressed bits of each input value that will be encoded.
+- BLOSC_CODEC_ZFP_FIXED_RATE: `meta` must be a number between 1 and 100. It is called *ratio* and represents the size that the compressed cells must have based on the input cell size. For example, if the cell size is 2000 bytes and meta = 50, the output cell size will be 50% of 2000 = 1000 bytes.
 
 To choose one of the compression modes, user must use its corresponding identifier as `blosc2_cparams.compcode`. For example:
 
     cparams.compcode = BLOSC_CODEC_ZFP_FIXED_ACCURACY;
-    
+
+or:
+
+    cparams.compcode = BLOSC_CODEC_ZFP_FIXED_PRECISION;    
 or:
 
     cparams.compcode = BLOSC_CODEC_ZFP_FIXED_RATE;
--- plugins/codecs/zfp/blosc2-zfp.c
+++ plugins/codecs/zfp/blosc2-zfp.c
@@ -197,6 +197,241 @@ int blosc2_zfp_acc_decompress(const uint8_t *input, int32_t input_len, uint8_t *
     return (int) output_len;
 }
 
+int blosc2_zfp_prec_compress(const uint8_t *input, int32_t input_len, uint8_t *output,
+                            int32_t output_len, uint8_t meta, blosc2_cparams *cparams) {
+    ZFP_ERROR_NULL(input);
+    ZFP_ERROR_NULL(output);
+    ZFP_ERROR_NULL(cparams);
+
+/*
+    printf("\n input \n");
+    for (int i = 0; i < input_len; i++) {
+        printf("%u, ", input[i]);
+    }
+*/
+    int8_t ndim;
+    int64_t *shape = malloc(8 * sizeof(int64_t));
+    int32_t *chunkshape = malloc(8 * sizeof(int32_t));
+    int32_t *blockshape = malloc(8 * sizeof(int32_t));
+    uint8_t *smeta;
+    uint32_t smeta_len;
+    if (blosc2_meta_get(cparams->schunk, "caterva", &smeta, &smeta_len) < 0) {
+        printf("Blosc error");
+        return -1;
+    }
+    deserialize_meta(smeta, smeta_len, &ndim, shape, chunkshape, blockshape);
+    free(smeta);
+
+    zfp_type type;     /* array scalar type */
+    zfp_field *field;  /* array meta data */
+    zfp_stream *zfp;   /* stream containing the real output buffer */
+    zfp_stream *zfp_aux;   /* auxiliar compressed stream */
+    bitstream *stream; /* bit stream to write to or read from */
+    bitstream *stream_aux; /* auxiliar bit stream to write to or read from */
+    size_t zfpsize;    /* byte size of compressed stream */
+
+    uint prec;
+    switch (ndim) {
+        case 1:
+            prec = meta + 5;
+            break;
+        case 2:
+            prec = meta + 7;
+            break;
+        case 3:
+            prec = meta + 9;
+            break;
+        case 4:
+            prec = meta + 11;
+            break;
+        default:
+            printf("\n ZFP is not available for this ndim \n");
+            return 0;
+    }
+
+    if(prec > ZFP_MAX_PREC) {
+        BLOSC_TRACE_ERROR("Max precision for this codecs is %d", ZFP_MAX_PREC);
+        prec = ZFP_MAX_PREC;
+    }
+
+    int32_t typesize = cparams->typesize;
+    switch (typesize) {
+        case sizeof(float):
+            type = zfp_type_float;
+            break;
+        case sizeof(double):
+            type = zfp_type_double;
+            break;
+        default:
+            printf("\n ZFP is not available for this typesize \n");
+            return 0;
+    }
+
+    zfp = zfp_stream_open(NULL);
+    zfp_stream_set_precision(zfp, prec);
+    stream = stream_open(output, output_len);
+    zfp_stream_set_bit_stream(zfp, stream);
+    zfp_stream_rewind(zfp);
+
+    switch (ndim) {
+        case 1:
+            field = zfp_field_1d((void *) input, type, blockshape[0]);
+            break;
+        case 2:
+            field = zfp_field_2d((void *) input, type, blockshape[1], blockshape[0]);
+            break;
+        case 3:
+            field = zfp_field_3d((void *) input, type, blockshape[2], blockshape[1], blockshape[0]);
+            break;
+        case 4:
+            field = zfp_field_4d((void *) input, type, blockshape[3], blockshape[2], blockshape[1], blockshape[0]);
+            break;
+        default:
+            printf("\n ZFP is not available for this number of dims \n");
+            return 0;
+    }
+
+    int zfp_maxout = (int) zfp_stream_maximum_size(zfp, field);
+    zfp_stream_close(zfp);
+    stream_close(stream);
+    uint8_t *aux_out = malloc(zfp_maxout);
+    zfp_aux = zfp_stream_open(NULL);
+    zfp_stream_set_precision(zfp_aux, prec);
+    stream_aux = stream_open(aux_out, zfp_maxout);
+    zfp_stream_set_bit_stream(zfp_aux, stream_aux);
+    zfp_stream_rewind(zfp_aux);
+
+    zfpsize = zfp_compress(zfp_aux, field);
+
+    /* clean up */
+    zfp_field_free(field);
+    zfp_stream_close(zfp_aux);
+    stream_close(stream_aux);
+    free(shape);
+    free(chunkshape);
+    free(blockshape);
+
+    if (zfpsize < 0) {
+        BLOSC_TRACE_ERROR("\n ZFP: Compression failed\n");
+        free(aux_out);
+        return (int) zfpsize;
+    }
+    if (zfpsize >= input_len) {
+        BLOSC_TRACE_ERROR("\n ZFP: Compressed data is bigger than input! \n");
+        free(aux_out);
+        return 0;
+    }
+
+    memcpy(output, aux_out, zfpsize);
+    free(aux_out);
+
+    return (int) zfpsize;
+}
+
+int blosc2_zfp_prec_decompress(const uint8_t *input, int32_t input_len, uint8_t *output,
+                              int32_t output_len, uint8_t meta, blosc2_dparams *dparams) {
+    ZFP_ERROR_NULL(input);
+    ZFP_ERROR_NULL(output);
+    ZFP_ERROR_NULL(dparams);
+
+    int8_t ndim;
+    int64_t *shape = malloc(8 * sizeof(int64_t));
+    int32_t *chunkshape = malloc(8 * sizeof(int32_t));
+    int32_t *blockshape = malloc(8 * sizeof(int32_t));
+    uint8_t *smeta;
+    uint32_t smeta_len;
+    if (blosc2_meta_get(dparams->schunk, "caterva", &smeta, &smeta_len) < 0) {
+        printf("Blosc error");
+        return -1;
+    }
+    deserialize_meta(smeta, smeta_len, &ndim, shape, chunkshape, blockshape);
+    free(smeta);
+
+    zfp_type type;     /* array scalar type */
+    zfp_field *field;  /* array meta data */
+    zfp_stream *zfp;   /* compressed stream */
+    bitstream *stream; /* bit stream to write to or read from */
+    size_t zfpsize;    /* byte size of compressed stream */
+
+    uint prec;
+    switch (ndim) {
+        case 1:
+            prec = meta + 5;
+            break;
+        case 2:
+            prec = meta + 7;
+            break;
+        case 3:
+            prec = meta + 9;
+            break;
+        case 4:
+            prec = meta + 11;
+            break;
+        default:
+            printf("\n ZFP is not available for this ndim \n");
+            return 0;
+    }
+
+    if(prec > ZFP_MAX_PREC) {
+        BLOSC_TRACE_ERROR("Max precision for this codecs is %d", ZFP_MAX_PREC);
+        prec = ZFP_MAX_PREC;
+    }
+
+    int32_t typesize = dparams->typesize;
+    switch (typesize) {
+        case sizeof(float):
+            type = zfp_type_float;
+            break;
+        case sizeof(double):
+            type = zfp_type_double;
+            break;
+        default:
+            printf("\n ZFP is not available for this typesize \n");
+            return 0;
+    }
+
+    zfp = zfp_stream_open(NULL);
+    zfp_stream_set_precision(zfp, prec);
+    stream = stream_open(input, input_len);
+    zfp_stream_set_bit_stream(zfp, stream);
+    zfp_stream_rewind(zfp);
+
+    switch (ndim) {
+        case 1:
+            field = zfp_field_1d((void *) output, type, blockshape[0]);
+            break;
+        case 2:
+            field = zfp_field_2d((void *) output, type, blockshape[1], blockshape[0]);
+            break;
+        case 3:
+            field = zfp_field_3d((void *) output, type, blockshape[2], blockshape[1], blockshape[0]);
+            break;
+        case 4:
+            field = zfp_field_4d((void *) output, type, blockshape[3], blockshape[2], blockshape[1], blockshape[0]);
+            break;
+        default:
+            printf("\n ZFP is not available for this number of dims \n");
+            return 0;
+    }
+
+    zfpsize = zfp_decompress(zfp, field);
+
+    /* clean up */
+    zfp_field_free(field);
+    zfp_stream_close(zfp);
+    stream_close(stream);
+    free(shape);
+    free(chunkshape);
+    free(blockshape);
+
+    if (zfpsize < 0) {
+        BLOSC_TRACE_ERROR("\n ZFP: Decompression failed\n");
+        return (int) zfpsize;
+    }
+
+    return (int) output_len;
+}
+
 int blosc2_zfp_rate_compress(const uint8_t *input, int32_t input_len, uint8_t *output,
                              int32_t output_len, uint8_t meta, blosc2_cparams *cparams) {
     ZFP_ERROR_NULL(input);
