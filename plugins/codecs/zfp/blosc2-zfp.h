a8bc67cffda6bfa1f56daa363c679bc28cb7b2f4
oscargm98 <soscargm98@gmail.com>
2022-03-03 19:01:00 +0100

    blosc_zfp_getcell() working for frames in-memory and on-disk (both sparse and contigous)

--- plugins/codecs/zfp/blosc2-zfp.h
+++ plugins/codecs/zfp/blosc2-zfp.h
@@ -18,6 +18,8 @@
 extern "C" {
 #endif
 
+#define ZFP_MAX_DIM 4
+
 int blosc2_zfp_acc_compress(const uint8_t *input, int32_t input_len, uint8_t *output, int32_t output_len,
                             uint8_t meta, blosc2_cparams *cparams, const void* chunk);
 
@@ -36,7 +38,11 @@ int blosc2_zfp_rate_compress(const uint8_t *input, int32_t input_len, uint8_t *o
 int blosc2_zfp_rate_decompress(const uint8_t *input, int32_t input_len, uint8_t *output, int32_t output_len,
                                uint8_t meta, blosc2_dparams *dparams, const void* chunk);
 
-int blosc2_zfp_decompress_cell(blosc2_schunk* schunk, int nchunk, int nblock, int ncell, void *dest, size_t destsize);
+int blosc2_zfp_getcell(blosc2_schunk* schunk, int nchunk, int nblock, int ncell, void *dest, size_t destsize);
+
+int blosc2_zfp_get_partial_block(blosc2_schunk* schunk, int nchunk, int nblock, bool* block_mask, int mask_len, void *dest, size_t destsize);
+
+int blosc2_zfp_get_block_slice(blosc2_schunk* schunk, int nchunk, int nblock, int64_t *start, int64_t *stop, void *dest, size_t destsize);
 
 #if defined (__cplusplus)
 }
