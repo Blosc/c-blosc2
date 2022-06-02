f0078f935c77184e4c20e3f78379e2807981ca63
oscargm98 <soscargm98@gmail.com>
2022-06-02 13:01:13 +0200

    index_unidim_to_multidim() moved to plugin_utils

--- plugins/codecs/zfp/zfp-private.h
+++ plugins/codecs/zfp/zfp-private.h
@@ -30,20 +30,6 @@ extern "C" {
     } while (0)
 
 
-static void index_unidim_to_multidim(uint8_t ndim, int32_t *shape, int64_t i, int64_t *index) {
-    int64_t strides[ZFP_MAX_DIM];
-    strides[ndim - 1] = 1;
-    for (int j = ndim - 2; j >= 0; --j) {
-        strides[j] = shape[j + 1] * strides[j + 1];
-    }
-
-    index[0] = i / strides[0];
-    for (int j = 1; j < ndim; ++j) {
-        index[j] = (i % strides[j - 1]) / strides[j];
-    }
-}
-
-
 #if defined (__cplusplus)
 }
 #endif
