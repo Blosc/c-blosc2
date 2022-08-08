2b57f5d0d2e3e816963ba1335c8ce346a24e2c61
Francesc Alted <francesc@blosc.org>
2022-08-08 19:29:40 +0200

    WIP -- more version symbols

--- tests/test_api.c
+++ tests/test_api.c
@@ -50,7 +50,7 @@ static char* test_cbuffer_versions(void) {
   int versionlz_;
 
   blosc1_cbuffer_versions(dest, &version_, &versionlz_);
-  mu_assert("ERROR: version incorrect", version_ == BLOSC_VERSION_FORMAT);
+  mu_assert("ERROR: version incorrect", version_ == BLOSC2_VERSION_FORMAT);
   mu_assert("ERROR: versionlz incorrect", versionlz_ == BLOSC_BLOSCLZ_VERSION_FORMAT);
   return 0;
 }
