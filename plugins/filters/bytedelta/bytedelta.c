99b749e6da79c41634c4f04a827597ae1dd0f489
Francesc Alted <francesc@blosc.org>
2023-03-08 15:00:58 +0100

    Use names wich are more C99 like

--- plugins/filters/bytedelta/bytedelta.c
+++ plugins/filters/bytedelta/bytedelta.c
@@ -30,18 +30,18 @@
 #endif
 
 #if CPU_ARCH_X64
-typedef __m128i Bytes16;
-Bytes16 SimdZero() { return _mm_setzero_si128(); }
-Bytes16 SimdSet1(uint8_t v) { return _mm_set1_epi8(v); }
-Bytes16 SimdLoad(const void* ptr) { return _mm_loadu_si128((const __m128i*)ptr); }
-void SimdStore(void* ptr, Bytes16 x) { _mm_storeu_si128((__m128i*)ptr, x); }
+typedef __m128i bytes16;
+bytes16 simd_zero() { return _mm_setzero_si128(); }
+bytes16 simd_set1(uint8_t v) { return _mm_set1_epi8(v); }
+bytes16 simd_load(const void* ptr) { return _mm_loadu_si128((const __m128i*)ptr); }
+void simd_store(void* ptr, bytes16 x) { _mm_storeu_si128((__m128i*)ptr, x); }
 
-Bytes16 SimdConcat(Bytes16 hi, Bytes16 lo) { return _mm_alignr_epi8(hi, lo, 15); }
-Bytes16 SimdAdd(Bytes16 a, Bytes16 b) { return _mm_add_epi8(a, b); }
-Bytes16 SimdSub(Bytes16 a, Bytes16 b) { return _mm_sub_epi8(a, b); }
-Bytes16 SimdShuffle(Bytes16 x, Bytes16 table) { return _mm_shuffle_epi8(x, table); }
+bytes16 simd_concat(bytes16 hi, bytes16 lo) { return _mm_alignr_epi8(hi, lo, 15); }
+bytes16 simd_add(bytes16 a, bytes16 b) { return _mm_add_epi8(a, b); }
+bytes16 simd_sub(bytes16 a, bytes16 b) { return _mm_sub_epi8(a, b); }
+bytes16 simd_shuffle(bytes16 x, bytes16 table) { return _mm_shuffle_epi8(x, table); }
 
-Bytes16 SimdPrefixSum(Bytes16 x)
+bytes16 simd_prefix_sum(bytes16 x)
 {
   // Sklansky-style sum from https://gist.github.com/rygorous/4212be0cd009584e4184e641ca210528
   x = _mm_add_epi8(x, _mm_slli_epi64(x, 8));
@@ -52,21 +52,21 @@ Bytes16 SimdPrefixSum(Bytes16 x)
 }
 
 #elif CPU_ARCH_ARM64
-typedef uint8x16_t Bytes16;
-Bytes16 SimdZero() { return vdupq_n_u8(0); }
-Bytes16 SimdSet1(uint8_t v) { return vdupq_n_u8(v); }
-Bytes16 SimdLoad(const void* ptr) { return vld1q_u8((const uint8_t*)ptr); }
-void SimdStore(void* ptr, Bytes16 x) { vst1q_u8((uint8_t*)ptr, x); }
+typedef uint8x16_t bytes16;
+bytes16 simd_zero() { return vdupq_n_u8(0); }
+bytes16 simd_set1(uint8_t v) { return vdupq_n_u8(v); }
+bytes16 simd_load(const void* ptr) { return vld1q_u8((const uint8_t*)ptr); }
+void simd_store(void* ptr, bytes16 x) { vst1q_u8((uint8_t*)ptr, x); }
 
-Bytes16 SimdConcat(Bytes16 hi, Bytes16 lo) { return vextq_u8(lo, hi, 15); }
-Bytes16 SimdAdd(Bytes16 a, Bytes16 b) { return vaddq_u8(a, b); }
-Bytes16 SimdSub(Bytes16 a, Bytes16 b) { return vsubq_u8(a, b); }
-Bytes16 SimdShuffle(Bytes16 x, Bytes16 table) { return vqtbl1q_u8(x, table); }
+bytes16 simd_concat(bytes16 hi, bytes16 lo) { return vextq_u8(lo, hi, 15); }
+bytes16 simd_add(bytes16 a, bytes16 b) { return vaddq_u8(a, b); }
+bytes16 simd_sub(bytes16 a, bytes16 b) { return vsubq_u8(a, b); }
+bytes16 simd_shuffle(bytes16 x, bytes16 table) { return vqtbl1q_u8(x, table); }
 
-Bytes16 SimdPrefixSum(Bytes16 x)
+bytes16 simd_prefix_sum(bytes16 x)
 {
   // Kogge-Stone-style like commented out part of https://gist.github.com/rygorous/4212be0cd009584e4184e641ca210528
-  Bytes16 zero = vdupq_n_u8(0);
+  bytes16 zero = vdupq_n_u8(0);
   x = vaddq_u8(x, vextq_u8(zero, x, 16 - 1));
   x = vaddq_u8(x, vextq_u8(zero, x, 16 - 2));
   x = vaddq_u8(x, vextq_u8(zero, x, 16 - 4));
@@ -95,13 +95,13 @@ int bytedelta_encoder(const uint8_t *input, uint8_t *output, int32_t length, uin
   const int stream_len = length / typesize;
   for (int ich = 0; ich < typesize; ++ich) {
     // delta within each channel, store
-    Bytes16 v2 = {0};
+    bytes16 v2 = {0};
     int ip = 0;
     for (; ip < stream_len - 15; ip += 16) {
-      Bytes16 v = SimdLoad(input);
+      bytes16 v = simd_load(input);
       input += 16;
-      Bytes16 delta = SimdSub(v, SimdConcat(v, v2));
-      SimdStore(output, delta);
+      bytes16 delta = simd_sub(v, simd_concat(v, v2));
+      simd_store(output, delta);
       output += 16;
       v2 = v;
     }
@@ -134,18 +134,18 @@ int bytedelta_decoder(const uint8_t *input, uint8_t *output, int32_t length, uin
     typesize = schunk->typesize;
   }
 
-  const Bytes16 hibyte = SimdSet1(15);
+  const bytes16 hibyte = simd_set1(15);
   const int stream_len = length / typesize;
   for (int ich = 0; ich < typesize; ++ich) {
     // fetch 16 bytes from each channel, prefix-sum un-delta
-    Bytes16 v2 = {0};
+    bytes16 v2 = {0};
     int ip = 0;
     for (; ip < stream_len - 15; ip += 16) {
-      Bytes16 v = SimdLoad(input);
+      bytes16 v = simd_load(input);
       input += 16;
       // un-delta via prefix sum
-      v2 = SimdAdd(SimdPrefixSum(v), SimdShuffle(v2, hibyte));
-      SimdStore(output, v2);
+      v2 = simd_add(simd_prefix_sum(v), simd_shuffle(v2, hibyte));
+      simd_store(output, v2);
       output += 16;
     }
     // leftover (should never happen, but anyway)
