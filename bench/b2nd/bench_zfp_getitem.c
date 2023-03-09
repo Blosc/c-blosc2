bf2363392241dedbc23d55cdad9bd81cf923e2e6
Francesc Alted <francesc@blosc.org>
2023-03-09 12:29:07 +0100

    Updated script for downloading data

--- bench/b2nd/bench_zfp_getitem.c
+++ bench/b2nd/bench_zfp_getitem.c
@@ -16,12 +16,10 @@
 
 8<---snip---- "fetch_data.py"
 #!/usr/bin/env python
-import os
-import sys
-import xarray as xr
 import numpy as np
 import s3fs
-import b2nd as cat
+import xarray as xr
+import blosc2
 
 def open_zarr(year, month, datestart, dateend):
     fs = s3fs.S3FileSystem(anon=True)
@@ -29,24 +27,14 @@ def open_zarr(year, month, datestart, dateend):
     s3map = s3fs.S3Map(datestring + "precipitation_amount_1hour_Accumulation.zarr/", s3=fs)
     precip_zarr = xr.open_dataset(s3map, engine="zarr")
     precip_zarr = precip_zarr.sel(time1=slice(np.datetime64(datestart), np.datetime64(dateend)))
-
     return precip_zarr.precipitation_amount_1hour_Accumulation
 
 print("Fetching data from S3 (era5-pds)...")
 precip_m0 = open_zarr(1987, 10, "1987-10-01", "1987-10-30 23:59")
-
-if os.path.exists("precip1.cat"):
-    cat.remove(path)
-
-# ia.set_config_defaults(favor=ia.Favor.SPEED)
-m_shape = precip_m0.shape
-m_chunks = (128, 128, 256)
-m_blocks = (32, 32, 32)
-cat_precip0 = cat.empty(m_shape, itemsize=4, chunks=m_chunks, blocks=m_blocks,
-                        urlpath="precip1.cat", contiguous=True)
+precip0 = blosc2.empty(shape=precip_m0.shape, dtype=precip_m0.dtype, urlpath="precip1.b2nd")
 print("Fetching and storing 1st month...")
 values = precip_m0.values
-cat_precip0[:] = values
+precip0[:] = values
 8<---snip----
 
  * To call this script, you can run the following commands:
