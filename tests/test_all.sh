f660a34eef309a62fbf64fb627010051a67fdb92
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2023-04-22 10:13:00 +0300

    Standardise copyright/licence header

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -3,7 +3,9 @@
 #
 #  Unit tests for basic features in Blosc.
 #
-#  Author: The Blosc Development team <blosc@blosc.org>
+#  Copyright (c) 2010  The Blosc Development team <blosc@blosc.org>
+#  https://blosc.org
+#  License: BSD 3-Clause (see LICENSE.txt)
 #
 #  See LICENSE.txt for details about copyright and rights to use.
 #**********************************************************************
