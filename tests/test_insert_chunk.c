849ed7cbe2b9fee543d6ac0f476b7ae96feb3291
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2023-05-14 12:50:16 +0200

    Define and use the ARRAY_SIZE macro in tests
    The resulting code is more readable.
    
    Borrowed from the Linux kernel (include/linux/kernel.h).
    ARRAY_SIZE(x) returns the number of elements in array x.

--- tests/test_insert_chunk.c
+++ tests/test_insert_chunk.c
@@ -170,9 +170,9 @@ static char* test_insert_chunk(void) {
 
 static char *all_tests(void) {
 
-  for (int i = 0; i < (int) (sizeof(tstorage) / sizeof(test_storage)); ++i) {
-    for (int j = 0; j < (int) (sizeof(tndata) / sizeof(test_ndata)); ++j) {
-      for (int k = 0; k < (int) (sizeof(tcopy) / sizeof(bool)); ++k) {
+  for (int i = 0; i < (int) ARRAY_SIZE(tstorage); ++i) {
+    for (int j = 0; j < (int) ARRAY_SIZE(tndata); ++j) {
+      for (int k = 0; k < (int) ARRAY_SIZE(tcopy); ++k) {
 
         tdata.contiguous = tstorage[i].contiguous;
         tdata.urlpath = tstorage[i].urlpath;
