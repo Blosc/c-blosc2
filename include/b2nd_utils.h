e94de3d7c59e0581ddef192cd82a9fd838bacc41
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2023-10-26 10:26:24 +0200

    Export the b2nd_copy_buffer function

--- include/b2nd_utils.h
+++ include/b2nd_utils.h
@@ -11,13 +11,29 @@
 #ifndef BLOSC_B2ND_UTILS_H
 #define BLOSC_B2ND_UTILS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "blosc2/blosc2-export.h"
+#ifdef __cplusplus
+}
+#endif
+
 #include <stdint.h>
 
-int b2nd_copy_buffer(int8_t ndim,
-                     uint8_t itemsize,
-                     void *src, const int64_t *src_pad_shape,
-                     int64_t *src_start, const int64_t *src_stop,
-                     void *dst, const int64_t *dst_pad_shape,
-                     int64_t *dst_start);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+BLOSC_EXPORT int b2nd_copy_buffer(int8_t ndim,
+                                  uint8_t itemsize,
+                                  void *src, const int64_t *src_pad_shape,
+                                  int64_t *src_start, const int64_t *src_stop,
+                                  void *dst, const int64_t *dst_pad_shape,
+                                  int64_t *dst_start);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* BLOSC_B2ND_UTILS_H */
