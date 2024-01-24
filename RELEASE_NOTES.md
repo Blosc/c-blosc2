c6e8bf60a991c57b0c3d4b887dfb3a414ecd7221
Marta Iborra <martaiborra24@gmail.com>
2024-01-24 10:47:32 +0100

    Post 2.13.0 release actions done

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,6 +1,12 @@
 Release notes for C-Blosc2
 ==========================
 
+Changes from 2.13.0 to 2.13.1
+=============================
+
+#XXX version-specific blurb XXX#
+
+
 Changes from 2.12.0 to 2.13.0
 =============================
 
--- include/blosc2.h
+++ include/blosc2.h
@@ -83,9 +83,9 @@ extern "C" {
 /* Version numbers */
 #define BLOSC2_VERSION_MAJOR    2    /* for major interface/format changes  */
 #define BLOSC2_VERSION_MINOR    13   /* for minor interface/format changes  */
-#define BLOSC2_VERSION_RELEASE  0    /* for tweaks, bug-fixes, or development */
+#define BLOSC2_VERSION_RELEASE  1    /* for tweaks, bug-fixes, or development */
 
-#define BLOSC2_VERSION_STRING   "2.13.0"  /* string version.  Sync with above! */
+#define BLOSC2_VERSION_STRING   "2.13.1.dev"  /* string version.  Sync with above! */
 #define BLOSC2_VERSION_DATE     "$Date:: 2023-01-24 #$"    /* date version */
 
 
