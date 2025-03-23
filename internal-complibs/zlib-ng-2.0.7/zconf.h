15041cf7afc85fee36e97738d5840b5b3aeb714e
emild <emildohne@gmail.com>
2025-03-23 16:46:58 +0100

    revert zconf.h

--- internal-complibs/zlib-ng-2.0.7/zconf.h
+++ internal-complibs/zlib-ng-2.0.7/zconf.h
@@ -135,7 +135,7 @@ typedef void       *voidp;
 
 typedef unsigned int z_crc_t;
 
-#if 0    /* was set to #if 0 by configure/cmake/etc */
+#if 1    /* was set to #if 1 by configure/cmake/etc */
 #  define Z_HAVE_UNISTD_H
 #endif
 
