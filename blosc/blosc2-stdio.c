2abd2fafa0a495a152937e7d2ba91e00ab1d0728
emild <emildohne@gmail.com>
2025-03-23 15:09:25 +0100

    fix unused variable warning (only used on linux branch)

--- blosc/blosc2-stdio.c
+++ blosc/blosc2-stdio.c
@@ -376,10 +376,11 @@ int64_t blosc2_stdio_mmap_write(const void *ptr, int64_t size, int64_t nitems, i
   }
 
   if (mmap_file->mapping_size < mmap_file->file_size) {
-    int64_t old_mapping_size = mmap_file->mapping_size;
     mmap_file->mapping_size = mmap_file->file_size * 2;
 
 #if defined(__linux__)
+    int64_t old_mapping_size = mmap_file->mapping_size;
+
     /* mremap is the best option as it also ensures that the old data is still available in c mode. Unfortunately, it
     is no POSIX standard and only available on Linux */
     char* new_address = mremap(mmap_file->addr, old_mapping_size, mmap_file->mapping_size, MREMAP_MAYMOVE);
