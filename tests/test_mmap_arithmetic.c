2ddee5c5f5eebb902ecaa73735549a479246f31d
metsw24-max <metsw24@gmail.com>
2026-04-17 17:16:01 +0530

    updated

--- tests/test_mmap_arithmetic.c
+++ tests/test_mmap_arithmetic.c
@@ -17,13 +17,11 @@ CUTEST_TEST_DATA(mmap_arithmetic) {
 };
 
 CUTEST_TEST_SETUP(mmap_arithmetic) {
-CUTEST_TEST_SETUP(mmap_arithmetic) {
   BLOSC_UNUSED_PARAM(data);
   blosc2_init();
 }
 
 CUTEST_TEST_TEST(mmap_arithmetic) {
-  BLOSC_UNUSED_PARAM(data);
   BLOSC_UNUSED_PARAM(data);
   uint8_t backing[32];
   memset(backing, 0, sizeof(backing));
