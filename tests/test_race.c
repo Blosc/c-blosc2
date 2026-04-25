1470f26afe2fd63a2d9359d9e4e70e6804d3566f
metsw24-max <metsw24@gmail.com>
2026-04-25 03:43:56 +0000

    updated

--- tests/test_race.c
+++ tests/test_race.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include "blosc2.h"
 
 void* set_compressor_thread(void* arg) {
@@ -12,12 +11,12 @@ void* set_compressor_thread(void* arg) {
 int main() {
     blosc2_init();
 
-    pthread_t t1, t2;
-    pthread_create(&t1, NULL, set_compressor_thread, "blosclz");
-    pthread_create(&t2, NULL, set_compressor_thread, "lz4");
+    blosc2_pthread_t t1, t2;
+    blosc2_pthread_create(&t1, NULL, set_compressor_thread, "blosclz");
+    blosc2_pthread_create(&t2, NULL, set_compressor_thread, "lz4");
 
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
+    blosc2_pthread_join(t1, NULL);
+    blosc2_pthread_join(t2, NULL);
 
     const char* comp = blosc1_get_compressor();
     printf("Final compressor: %s\n", comp);
