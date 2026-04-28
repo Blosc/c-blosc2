8601d675c1cd4cccb675769b1d0fef22a5165c63
metsw24-max <metsw24@gmail.com>
2026-04-28 04:33:20 +0000

    updated

--- tests/test_race.c
+++ tests/test_race.c
@@ -14,9 +14,25 @@ struct set_compressor_arg {
     int rc;
 };
 
+struct get_compressor_arg {
+    volatile int stop;
+    int errors;
+};
+
 void* set_compressor_thread(void* arg) {
     struct set_compressor_arg* state = (struct set_compressor_arg*)arg;
     state->rc = blosc1_set_compressor(state->comp);
+    return NULL;
+}
+
+void* get_compressor_thread(void* arg) {
+    struct get_compressor_arg* state = (struct get_compressor_arg*)arg;
+    while (!state->stop) {
+        const char* comp = blosc1_get_compressor();
+        if (!is_expected_compressor(comp)) {
+            state->errors++;
+        }
+    }
     return NULL;
 }
 
@@ -27,12 +43,20 @@ int main() {
     blosc2_init();
 
     for (i = 0; i < iterations; ++i) {
-        blosc2_pthread_t t1, t2;
+        blosc2_pthread_t t1, t2, treader;
         int rc;
         const char* comp;
 
         struct set_compressor_arg arg1 = {"blosclz", 0};
         struct set_compressor_arg arg2 = {"lz4", 0};
+        struct get_compressor_arg reader_arg = {0, 0};
+
+        rc = blosc2_pthread_create(&treader, NULL, get_compressor_thread, &reader_arg);
+        if (rc != 0) {
+            fprintf(stderr, "blosc2_pthread_create for reader failed on iteration %d: %d\n", i, rc);
+            blosc2_destroy();
+            return 1;
+        }
 
         rc = blosc2_pthread_create(&t1, NULL, set_compressor_thread, &arg1);
         if (rc != 0) {
@@ -60,6 +84,22 @@ int main() {
         rc = blosc2_pthread_join(t2, NULL);
         if (rc != 0) {
             fprintf(stderr, "blosc2_pthread_join for t2 failed on iteration %d: %d\n", i, rc);
+            reader_arg.stop = 1;
+            blosc2_pthread_join(treader, NULL);
+            blosc2_destroy();
+            return 1;
+        }
+
+        reader_arg.stop = 1;
+        rc = blosc2_pthread_join(treader, NULL);
+        if (rc != 0) {
+            fprintf(stderr, "blosc2_pthread_join for reader failed on iteration %d: %d\n", i, rc);
+            blosc2_destroy();
+            return 1;
+        }
+        if (reader_arg.errors != 0) {
+            fprintf(stderr, "blosc1_get_compressor() returned invalid value %d times on iteration %d\n",
+                    reader_arg.errors, i);
             blosc2_destroy();
             return 1;
         }
